c32214058abec8cefbaf087298f262aeb7ef4bfa
Matt Olson <molson@paybyphone.com>
2017-02-09 14:38:32 -0800

    WordWrap kata: Base solution

---
+++ WordWrap/WordWrap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace WordWrap
+{
+    public static class WordWrap
+    {
+        public static string Wrap(string input, int maxLength)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    [TestFixture]
+    public class WordWrapTests
+    {
+        [Test]
+        public void write_your_first_test_here()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}

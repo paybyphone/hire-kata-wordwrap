aac53788f33a66454b0d15f7ed001a093dcae222
Candidate PayByPhone <pbphire@georgekronberg.com>
2017-02-16 22:47:51 +0000

    autocommit on change

--- WordWrap/WordWrap.cs
+++ WordWrap/WordWrap.cs
@@ -5,7 +5,11 @@ namespace WordWrap
 {
     public static class WordWrap
     {
-        public static string Wrap(string input, int maxLength)
+        // Implement a word-wrapping function that, given a string of words separated by spaces,
+        //  breaks that string into multiple lines such that no line is longer than the maximum
+        //  length.
+        // For example, WordWrap.Wrap("This is a test", 7) should return "This is\na test"
+        public static string Wrap(string input, int maxLineLength)
         {
             throw new NotImplementedException();
         }

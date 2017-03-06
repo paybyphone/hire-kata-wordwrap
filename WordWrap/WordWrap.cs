721f8db7f3f8fb98eafeb8a9e260aa4e6b64492a
Candidate PayByPhone <pbphire@georgekronberg.com>
2017-03-06 22:04:03 +0000

    autocommit on change

--- WordWrap/WordWrap.cs
+++ WordWrap/WordWrap.cs
@@ -32,7 +32,7 @@ namespace WordWrap
             //  Assert.That(someInt,        Is.LessThan(42));
             //  Assert.That(someCollection, Is.Not.Empty);
             Assert.That(true, Is.True);
-            Assert.That(WordWrap.Warp("", 5), Is.EqualTo(""));
+            Assert.That(WordWrap.Wrap("", 5), Is.EqualTo(""));
         }
     }
 }
